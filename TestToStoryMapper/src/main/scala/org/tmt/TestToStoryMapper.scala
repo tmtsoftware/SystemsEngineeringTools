b5511b224846768061f07a75509abe027fb72da0
weiss <weiss.personal@gmail.com>
2018-05-11 13:17:59 -0700

    Added Github links, Story to Test map, read/dump to tab delimited file

--- TestToStoryMapper/src/main/scala/org/tmt/TestToStoryMapper.scala
+++ TestToStoryMapper/src/main/scala/org/tmt/TestToStoryMapper.scala
@@ -2,30 +2,111 @@ package org.tmt
 
 import scala.io.Source
 import scala.collection.immutable.{ListMap, Set}
-import java.io.File
+import java.io.{File, PrintWriter}
 // rules:
 //
 // if USR (user story reference) comes before class, USR applies to all tests.
 // if USR just above test (no lines between USR and test declaration), only applies to that test
 // if USR is within a test, it only applies to that test.
 
+case class TestFile(relativePath: String, filename: String)
 case class UserStoryReference(reference: String)
-case class TestReference(filename: String, className: String, testName: String)
+case class TestReference(file: TestFile, className: String, testName: String, lineNumber: Int)
+// TODO add URL to github for test (at line number if possible)
+// TODO csw.services.config.server.ConfigServerTest is an abstract class. does this need special handling?
 
 object TestToStoryMapper extends App {
-  var testMap = ListMap[TestReference, Set[UserStoryReference]]()
+  val githubPath = "https://github.com/tmtsoftware"
+  var project = ""
+  var rootDir = "."
+  val delim="\t"
+  val header=s"path${delim}lineNumber${delim}githubLink${delim}className${delim}testName${delim}userStories"
 
-  def getReference(line:String) = UserStoryReference(line.drop(line.indexOf("DEOPSCSW")).takeWhile(testEnd))
+  if (args.length == 0) {
+    // return error
+    println("USAGE: TestToStoryMapper project <rootDir>")
+    System.exit(-1)
+  } else {
+    project = args(0)
+    if (args.length > 1) {
+      rootDir = args(1)
+    }
+  }
+
+  def recursiveListFiles(f: File): Array[File] = {
+    val these = f.listFiles
+    these ++ these.filter(_.isDirectory).flatMap(recursiveListFiles)
+  }
+
+  val testFiles = {
+    recursiveListFiles(new File(rootDir+File.separator+project))
+      .filter(_.isFile)
+      .map(x => TestFile(x.toString.drop(rootDir.length+File.separator.length+project.length+File.separator.length), x.getCanonicalPath))
+      .filter(_.filename.contains("/test/"))
+      .filter(!_.filename.contains("/resources/"))
+      .filter(!_.filename.contains("/target/"))
+  }
+
+  def getReference(line:String) = UserStoryReference("DEOPSCSW-"+line.drop(line.indexOf("DEOPSCSW")+9).takeWhile(testEnd))
+  def getGithubLink(t: TestReference) = s"$githubPath/$project/blob/master/${t.file.relativePath}#L${t.lineNumber}"
+
+
+  var testMap = ListMap[TestReference, Set[UserStoryReference]]()
 
   def printMap(): Unit = {
     for ((t,s) <- testMap) {
-      println(s"${t.filename}: ${t.className} - ${t.testName} ->")
+      println(s"${getGithubLink(t)}: ${t.lineNumber} (${t.className}) - ${t.testName} ->")
       s.foreach(x => println(s"-- ${x.reference}"))
     }
   }
 
+  def dumpCSV(file: File) = {
+    val writer = new PrintWriter(file)
+    writer.write(s"$header\n")
+    for ((t,s) <- testMap) {
+      writer.write(s"${t.file.relativePath}$delim${t.lineNumber}$delim${getGithubLink(t)}$delim${t.className}$delim${t.testName}$delim[${s.map(_.reference).mkString(",")}]\n")
+    }
+    writer.close()
+  }
+
+  def openCSV(file: File) = {
+    val source = Source.fromFile(file)
+    for (line <- source.getLines()) {
+      if (!line.startsWith(header) && (line.length > 0)) {
+        val parts = line.split(delim)
+        val file = new File(s"$rootDir${File.separator}${parts(0)}")
+        val testFile = TestFile(parts(0), file.getCanonicalPath)
+        val testRef = TestReference(testFile,
+          parts(3),
+          parts(4)   // replace double quote with single quote, eliminate single quotes.  this artifact is created by excel, and may be removed.
+            .replace("\"\"", "|")
+            .replace("\"","")
+            .replace("|", "\""),
+          parts(1).toInt)
+
+
+        val userStories = {
+          if (parts(5).contains("[]"))
+            Set[UserStoryReference]()
+          else
+            parts(5)   // quotes are an artifact from creating file from excel
+            .replace("\"", "")
+            .replace("[", "")
+            .replace("]","")
+            .split(",")
+            .map(UserStoryReference)
+            .toSet
+        }
+        addToMap(Some(testRef), userStories)
+      }
+    }
+  }
+
+
+
+
   def testEnd(x:Char) = {
-    (x != ':') && (x != ' ')
+    (x != ':') && (x != ' ') && (x != '-')
   }
 
   def addToMap(testRef: Option[TestReference], storyRefs: Set[UserStoryReference]): Unit = {
@@ -35,19 +116,24 @@ object TestToStoryMapper extends App {
   def addGlobalsToMap(testRef: Option[TestReference], storyRefs: Set[UserStoryReference]): Unit = {
     testMap += (testRef.get -> storyRefs)
   }
-  def processScalaFile(filename:String): Unit = {
+  def processScalaFile(file: TestFile): Unit = {
     var globalRefs = Set[UserStoryReference]()
     var testRefs = Set[UserStoryReference]()
     var lastTestReference: Option[TestReference] = None
 
     def getClassName(line:String) = line.drop(6).takeWhile(_ != ' ')
+    def getAbstractClassName(line:String) = line.drop(15).takeWhile(_ != ' ')
     def getTestName(line:String) = line.drop(line.indexOf("test(")).takeWhile(_ != ')')+')'
 
-    val source = Source.fromFile(filename)
+    val source = Source.fromFile(file.filename)
     var className: Option[String] = None
+    var lineNumber = 0
     for (line <- source.getLines()) {
+      lineNumber += 1
       if (line.startsWith("class")) {
         className = Some(getClassName(line))
+      } else if (line.startsWith("abstract class")) {
+          className = Some(getAbstractClassName(line))
       } else if (line.contains("DEOPSCSW")) {
         if (className.isEmpty) {
           globalRefs += getReference(line)
@@ -55,7 +141,7 @@ object TestToStoryMapper extends App {
           testRefs += getReference(line)
         }
       } else if (line.dropWhile(_ == ' ').startsWith("test(\"")) {
-        lastTestReference = Some(TestReference(filename, className.getOrElse("None"), getTestName(line)))
+        lastTestReference = Some(TestReference(file, className.getOrElse("None"), getTestName(line), lineNumber))
         addGlobalsToMap(lastTestReference, globalRefs)
         addToMap(lastTestReference, testRefs)
         testRefs = Set[UserStoryReference]()
@@ -69,20 +155,25 @@ object TestToStoryMapper extends App {
     source.close
   }
 
-  def processJavaFile(filename:String): Unit = {
+  def processJavaFile(file: TestFile): Unit = {
     var globalRefs = Set[UserStoryReference]()
     var testRefs = Set[UserStoryReference]()
     var lastTestReference: Option[TestReference] = None
     var testStart = false
 
     def getClassName(line:String) = line.drop(13).takeWhile(_ != ' ')
+    def getAbstractClassName(line:String) = line.drop(22).takeWhile(_ != ' ')
     def getTestName(line:String) = line.dropWhile(_ == ' ').drop(12).takeWhile(_ != '(')
 
-    val source = Source.fromFile(filename)
+    val source = Source.fromFile(file.filename)
     var className: Option[String] = None
+    var lineNumber = 0
     for (line <- source.getLines()) {
+      lineNumber += 1
       if (line.startsWith("public class")) {
         className = Some(getClassName(line))
+      } else if (line.startsWith("public abstract class")) {
+        className = Some(getAbstractClassName(line))
       } else if (line.contains("DEOPSCSW")) {
         if (className.isEmpty) {
           globalRefs += getReference(line)
@@ -92,7 +183,7 @@ object TestToStoryMapper extends App {
       } else if (line.dropWhile(_ == ' ').startsWith("@Test")) {
         testStart = true
       } else if (testStart) {
-        lastTestReference = Some(TestReference(filename, className.getOrElse("None"), getTestName(line)))
+        lastTestReference = Some(TestReference(file, className.getOrElse("None"), getTestName(line), lineNumber))
         addGlobalsToMap(lastTestReference, globalRefs)
         addToMap(lastTestReference, testRefs)
         testRefs = Set[UserStoryReference]()
@@ -106,25 +197,20 @@ object TestToStoryMapper extends App {
     }
     source.close
   }
-  def recursiveListFiles(f: File): Array[File] = {
-    val these = f.listFiles
-    these ++ these.filter(_.isDirectory).flatMap(recursiveListFiles)
-  }
 
-  def testFilenames(rootDir: String): Array[String] = {
-    recursiveListFiles(new File(rootDir))
-      .filter(_.isFile)
-      .map(x => x.getCanonicalPath)
-      .filter(_.contains("/test/"))
-      .filter(!_.contains("/resources/"))
-  }
-
-  val testFiles = testFilenames("../csw-prod/")
-
-
-  testFiles.filter(_.endsWith(".scala")).foreach(processScalaFile)
-  testFiles.filter(_.endsWith(".java")).foreach(processJavaFile)
+  openCSV(new File("/Users/Weiss/acceptTest/in.txt"))
+//  testFiles.filter(_.filename.endsWith(".scala")).foreach(processScalaFile)
+//  testFiles.filter(_.filename.endsWith(".java")).foreach(processJavaFile)
 
   printMap()
+
+  dumpCSV( new File("/Users/Weiss/acceptTest/test.csv"))
+
+  val inverseMap = testMap.toList.flatMap{ case (a,b) => b.map(_ -> a) }.groupBy(_._1).mapValues(_.map(_._2))
+
+  for ((t,s) <- inverseMap.toSeq.sortBy(_._1.reference.drop(9).toInt)) {
+    println(s"${t.reference}: ->")
+    s.foreach(x => println(s"-- ${x.className} - ${x.testName}"))
+  }
 
 }
