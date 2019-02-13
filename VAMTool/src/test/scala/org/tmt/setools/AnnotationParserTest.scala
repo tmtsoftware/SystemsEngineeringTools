e79354ddd4daf825ac24e20c95d644257a517911
weiss <weiss.personal@gmail.com>
2019-02-13 14:33:38 -0800

    Make tests actual tests

--- VAMTool/src/test/scala/org/tmt/setools/AnnotationParserTest.scala
+++ VAMTool/src/test/scala/org/tmt/setools/AnnotationParserTest.scala
@@ -4,12 +4,18 @@ import org.scalatest.{FunSuite, Matchers}
 
 class AnnotationParserTest extends FunSuite with Matchers {
 
+  private val expectedMap = Map(
+    "should_do_it_first"  -> Set("DEOPSCSW-002: fake story 2", "DEOPSCSW-006: fake story 6", "DEOPSCSW-003: fake story 3", "DEOPSCSW-007: fake story 7"),
+    "should_do_it_second" -> Set("DEOPSCSW-002: fake story 2", "DEOPSCSW-006: fake story 6", "DEOPSCSW-004: fake story 4"),
+    "should_do_it_third"  -> Set("DEOPSCSW-002: fake story 2", "DEOPSCSW-006: fake story 6")
+  )
+
   test("should get annotations form java test") {
-    println(AnnotationParser.getTestToStoryMap(classOf[AnnotatedJavaTest]))
+    AnnotationParser.getTestToStoryMap(classOf[AnnotatedJavaTest]) shouldBe expectedMap
   }
 
   test("should get annotations form scala test") {
-    println(AnnotationParser.getTestToStoryMap(classOf[AnnotatedScalaTest]))
+    AnnotationParser.getTestToStoryMap(classOf[AnnotatedScalaTest]) shouldBe expectedMap
   }
 
 }
