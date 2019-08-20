f9a09e68957504b8c01b4dbe9ad82f66f99ff420
weiss <weiss.personal@gmail.com>
2019-08-20 13:51:37 -0700

    Added spreadsheet ID for new CCR16 DRD

--- VAMTool/src/main/scala/org/tmt/setools/DRDParser.scala
+++ VAMTool/src/main/scala/org/tmt/setools/DRDParser.scala
@@ -3,12 +3,14 @@ package org.tmt.setools
 import org.tmt.setools.Utilities.Requirement
 
 /**
- * Extracts data from a google doc: "CSW Unit Test Verification Matrix".
+ * Extracts data from a google doc: "CSW DRD CCR16".
  * See example doc below.
  */
 object DRDParser {
-  // Example: https://docs.google.com/spreadsheets/d/1XxGE2USwKohWxLuZJUCPBdqNc5sXvE8S
-  private val spreadsheetId = "180ZP3I9SnkOWjZycyR9Truf6Fszp1myfrLLnF-lCXsY"
+  // Example: https://docs.google.com/spreadsheets/d/1jzcBOOz2SoN9njfrm-V8BR7WWLwhOZoxZSv3CNRqsZ8
+  private val spreadsheetId_CCR15 = "180ZP3I9SnkOWjZycyR9Truf6Fszp1myfrLLnF-lCXsY"
+  private val spreadsheetId_CCR16 = "1jzcBOOz2SoN9njfrm-V8BR7WWLwhOZoxZSv3CNRqsZ8"
+  private val defaultSpreadsheet = spreadsheetId_CCR16
   private val sheetId       = "Sheet1"
   private val idColumn      = 'A'
   private val textColumn    = 'B'
@@ -16,7 +18,7 @@ object DRDParser {
   private val textColumnNum = textColumn - 'A'
 
 
-  def getRequirements(spreadsheetId: String = spreadsheetId): List[Requirement] = {
+  def getRequirements(spreadsheetId: String = defaultSpreadsheet): List[Requirement] = {
     SheetsAccess
       .getAllData(spreadsheetId, sheetId)
       .drop(1)
