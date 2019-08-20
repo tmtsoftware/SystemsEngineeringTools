2681855dd166d47b69aa8f4c22bba8ce22a90d19
weiss <weiss.personal@gmail.com>
2019-08-20 14:44:03 -0700

    Only add user story form text if those fields are supplied

--- VAMTool/src/main/scala/org/tmt/setools/Utilities.scala
+++ VAMTool/src/main/scala/org/tmt/setools/Utilities.scala
@@ -22,7 +22,9 @@ object Utilities {
   case class UserStoryReference(reference: String)
 
   case class UserStory(reference: UserStoryReference, service: String, asA: String, iWantTo: String, soThat: String) extends Ordered[UserStory] {
-    def getText = s"As a/an $asA, I want to $iWantTo so that $soThat"
+    private val asText = if (asA.isEmpty) "" else s"As a/an $asA, I want to "
+    private val soText = if (soThat.isEmpty) "" else s" so that $soThat"
+    def getText = s"$asText$iWantTo$soText"
     private def getNumber = reference.reference.drop(9).toInt
     def compare(that: UserStory): Int = this.getNumber - that.getNumber
   }
